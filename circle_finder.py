7da22003e2e919a61803180c4959b4529471ec15
CALLOW <joseph.callow@durham.ac.uk>
2019-10-23 11:44:39 +0100

    Finished circle_finder_2 and added to 1, renamed josh's code, began to find circles with strip.py.

--- circle_finder.py
+++ circle_finder.py
@@ -3,20 +3,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 CMB_DIST = 14000 #Mpc
+CELL_SIZE = 320 #Mpc
 
-def circle_finder(domain_scale, x, y):
-	d = domain_scale*(np.sqrt(x**2+y**2))
+def circle_finder(cell_size, x, y):
+	d = cell_size*(np.sqrt(x**2+y**2))
 	ang_rad = np.arccos(d/(2*CMB_DIST))
 	return ang_rad
 
-domain_scale = 320
-no_of_circles = int(CMB_DIST/domain_scale)
-x=0
-y=0
+def draw_circle(x, y, r, coords, angles):
+	for i in range(len(angles)):
+		coords[0] = x + r*np.cos(angles)
+		coords[1] = y + r*np.sin(angles)
+	return coords 
 
-for i in range(no_of_circles):
-	x+=1
-	print np.degrees(circle_finder(domain_scale, x, y))
+if __name__ == '__main__':
+
+	coords_0 = np.zeros((2, 361))
+	coords_1 = np.zeros((2, 361))
+	coords_40 = np.zeros((2, 361))
+	angles = np.radians(np.arange(0,361, 1))
+
+	circle_0 = draw_circle(0, 0, CMB_DIST, coords_0, angles)
+	circle_1 = draw_circle(1*CELL_SIZE, 0, CMB_DIST, coords_1, angles)
+	circle_40 = draw_circle(40*CELL_SIZE, 0, CMB_DIST, coords_40, angles)
+
+	no_of_circles = int(CMB_DIST/CELL_SIZE)*2
+	x=0
+	y=0
+
+	ang_rad = np.zeros(no_of_circles)
+
+	for i in range(no_of_circles):
+		ang_rad[i] = circle_finder(CELL_SIZE, x, y)
+		x+=1
 
 
-
+	fig, ax = plt.subplots(figsize = (12,8))
+	"""ax.plot(circle_0[0], circle_0[1])
+	ax.plot(circle_1[0], circle_1[1])
+	ax.plot(CMB_DIST*np.cos(ang_rad[1]), CMB_DIST*np.sin(ang_rad[1]), 'x', color = 'r')
+	ax.plot(CMB_DIST*np.cos(ang_rad[1]), -CMB_DIST*np.sin(ang_rad[1]), 'x', color = 'r')
+	ax.set_title('Angular radius = ' + str(np.degrees(ang_rad[1])) + "$^\circ$")"""
+	ax.plot(circle_0[0], circle_0[1])
+	ax.plot(circle_40[0], circle_40[1])
+	ax.plot(CMB_DIST*np.cos(ang_rad[40]), CMB_DIST*np.sin(ang_rad[40]), 'x', color = 'r')
+	ax.plot(CMB_DIST*np.cos(ang_rad[40]), -CMB_DIST*np.sin(ang_rad[40]), 'x', color = 'r')
+	ax.set_title('Angular radius = ' + str(np.degrees(ang_rad[40])) + "$^\circ$")
+	plt.tight_layout()
+	plt.show()
--- strip.py
+++ strip.py
@@ -1,15 +1,25 @@
 from __future__ import division
 import numpy as np
 import healpy as hp
+from circle_finder import circle_finder
 import matplotlib.pyplot as plt
 
-SMICA_MAP = hp.read_map("COM_CMB_IQU-smica_2048_R3.00_full.fits")
+
+SMICA_MAP = hp.fitsfunc.read_map("/opt/local/l4astro/rbbg94/cmb_maps/planck_data.fits")
 
 NSIDE=2048
+CMB_DIST = 14000
+CELL_SIZE = 320
 
-ipix_strip = hp.query_strip(NSIDE, np.radians(40), np.radians(50))
+ang_rad = circle_finder(CELL_SIZE, 1, 0)
 
-SMICA_MAP[ipix_strip] = SMICA_MAP.max()
+ipix_strip_1 = hp.query_strip(NSIDE, ang_rad-(np.pi/360), ang_rad+(np.pi/360))
+ipix_strip_2 = hp.query_strip(NSIDE, np.pi-ang_rad-(np.pi/360), np.pi-ang_rad+(np.pi/360))
+
+SMICA_MAP[ipix_strip_1] = SMICA_MAP.max()
+SMICA_MAP[ipix_strip_2] = SMICA_MAP.min()
 hp.mollview(SMICA_MAP)
 
 plt.show()
+
+
